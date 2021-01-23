25661e7f40a361dbe28ef34adcab0c1083043761
GabrielJacobina <sgabrieljacobina@gmail.com>
2021-01-23 14:45:54 -0300

    Configuring service for usuario

--- src/main/java/com/agenda/controllers/UsuarioController.java
+++ src/main/java/com/agenda/controllers/UsuarioController.java
@@ -1,16 +1,14 @@
 package com.agenda.controllers;
 
 import com.agenda.models.Usuario;
-import com.agenda.repositorys.UsuarioRepository;
 import com.agenda.services.UsuarioService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("usuarios")
@@ -18,51 +16,33 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class UsuarioController {
 
-    private final UsuarioRepository usuarioRepository;
-
     private final UsuarioService usuarioService;
 
     @GetMapping
-    public ResponseEntity<List<Usuario>> getAllUsuarios(){
-        List<Usuario> usuarios = usuarioRepository.findAll();
-        if (usuarios.isEmpty()) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-        return new ResponseEntity<>(usuarios, HttpStatus.OK);
+    public ResponseEntity<Page<Usuario>> getAllUsuarios(Pageable pageable){
+        return new ResponseEntity<>(usuarioService.findAll(pageable), HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Usuario> getOneUsuario(@PathVariable(value = "id") long id){
-        Optional<Usuario> usuarioO = usuarioRepository.findById(id);
-        if (usuarioO.isEmpty()){
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-        return new ResponseEntity<>(usuarioO.get(), HttpStatus.OK);
+        return new ResponseEntity<>(usuarioService.findById(id), HttpStatus.OK);
     }
 
     @PostMapping
     public ResponseEntity<Usuario> createUsuario(@Validated @RequestBody Usuario usuario){
-        return new ResponseEntity<>(usuarioRepository.save(usuario), HttpStatus.CREATED);
+        return new ResponseEntity<>(usuarioService.save(usuario), HttpStatus.CREATED);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<?> deleteUsuario(@PathVariable(value = "id") long id){
-        Optional<Usuario> usuarioO = usuarioRepository.findById(id);
-        if (usuarioO.isEmpty()){
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-        usuarioRepository.delete(usuarioO.get());
-        return new ResponseEntity<>(HttpStatus.OK);
+    public ResponseEntity<Void> deleteUsuario(@PathVariable(value = "id") long id){
+        usuarioService.delete(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
-    @PutMapping("/{id}")
-    public ResponseEntity<Usuario> editUsuario(@Validated @PathVariable(value = "id") long id, @RequestBody Usuario usuario) {
-        Optional<Usuario> usuarioO = usuarioRepository.findById(id);
-        if (usuarioO.isEmpty()){
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-        usuario.setId(usuarioO.get().getId());
-        return new ResponseEntity<>(usuarioRepository.save(usuario), HttpStatus.OK);
+    @PutMapping
+    public ResponseEntity<Usuario> replaceUsuario(@Validated @RequestBody Usuario usuario) {
+        usuarioService.replace(usuario);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
 }
--- src/main/java/com/agenda/services/UsuarioService.java
+++ src/main/java/com/agenda/services/UsuarioService.java
@@ -1,11 +1,16 @@
 package com.agenda.services;
 
+import com.agenda.models.Usuario;
 import com.agenda.repositorys.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Optional;
 
@@ -19,5 +24,27 @@ public class UsuarioService implements UserDetailsService {
     public UserDetails loadUserByUsername(String username){
         return Optional.ofNullable(usuarioRepository.findByUsername(username))
                 .orElseThrow(() -> new UsernameNotFoundException("Usuario não encontrado"));
+    }
+
+    public Page<Usuario> findAll(Pageable pageable) {
+        return usuarioRepository.findAll(pageable);
+    }
+
+    public Usuario findById(long id) {
+        return usuarioRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid Request"));
+    }
+
+    public Usuario save(Usuario usuario) {
+        return usuarioRepository.save(usuario);
+    }
+
+    public void delete(long id) {
+        usuarioRepository.delete(findById(id));
+    }
+
+    public void replace(Usuario usuario) {
+        Usuario savedUsuario = findById(usuario.getId());
+        usuarioRepository.save(usuario);
     }
 }
